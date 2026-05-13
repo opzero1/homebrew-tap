853c67facaf3de1649d028297cb87baea051ed94
afif <afif.alauddin@reap.hk>
2026-05-13 19:33:20 +0800

    Update GifBar cask to 0.2.0

--- Casks/gifbar.rb
+++ Casks/gifbar.rb
@@ -1,10 +1,10 @@
 cask "gifbar" do
-  version "0.1.1"
-  sha256 "2b88a0576e8de0cfd4ba2a2efdcf0bb90e436b196570d89f6dd1fffcfb511b82"
+  version "0.2.0"
+  sha256 "3f8fa228fb7dcfd4ce8227921e30da576fb660317b550d4178e55e11f93dcb45"
 
   url "https://github.com/opzero1/gif-bar/releases/download/v#{version}/GifBar-#{version}.zip"
   name "GifBar"
-  desc "Menu bar helper for turning GIF pages into Slack-friendly CDN GIF links"
+  desc "Menu bar helper for searching Giphy and copying CDN GIF links"
   homepage "https://github.com/opzero1/gif-bar"
 
   depends_on macos: ">= :sonoma"
